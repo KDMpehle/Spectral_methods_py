9713a7a0aabfbf47e9644c242e31d301cd3439f1
Khaya Mpehle <khaya.mpehle@gmail.com>
2019-04-20 18:59:34 +1000

    Chap 1 and 2 edits

--- chapter_one.py
+++ chapter_one.py
@@ -3,9 +3,8 @@ import numpy as np
 from scipy import sparse
 from scipy.linalg import toeplitz
 import matplotlib.pyplot as plt
-#Program one: fourth order finite difference convergence
-program_val = 1
-if program_val == 1:  
+program_val = 2
+if program_val == 1: #Program one: fourth order finite difference convergence 
     N_vals = [2**k for k in range (3,12)]
     fig = plt.figure(figsize= (16,8))
     ax = plt.axes()
@@ -25,9 +24,13 @@ if program_val == 1:
         #plot the maxmum difference error
         error = np.linalg.norm(D.dot(u)-uprime,np.inf)
         errorvec.append(error)
-    ax.loglog(N_vals,errorvec,'x')
+    ax.loglog(N_vals,errorvec,'--x',mew = 4,ms = 8)
+    ax.set_xlabel('N',fontsize= 14)
+    ax.set_ylabel(r'Error in the Infinity norm',fontsize = 14)
+    title = r'4th order finite difference of $\exp(\sin(x))$'
+    ax.set_title(title,fontsize = 18)
     plt.show()
-elif program_val == 2
+elif program_val == 2:
     #program two: spectral convergence
     N_vals = [2*k for k in range(1,20)]
     errorvec=[]
@@ -46,5 +49,9 @@ elif program_val == 2
         D= toeplitz(cols,rows)
         error = np.linalg.norm(D.dot(u) - uprime,np.inf)
         errorvec.append(error)
-    ax.loglog(N_vals,errorvec,'o',color='k')
+    ax.loglog(N_vals,errorvec,'--x',color='k', mew =4,ms=8)
+    title = " Error by spectral differentiation"
+    ax.set_title(title,fontsize = 18)
+    ax.set_xlabel("N",fontsize = 14)
+    ax.set_ylabel("Infinity norm error", fontsize = 10)
     plt.show()          
--- chapter_two.py
+++ chapter_two.py
@@ -2,17 +2,24 @@
 # in matlab, translated by Khaya Mpehle
 import numpy as np
 import matplotlib.pyplot as plt
+
 #program 3: The band-limited interpolation (BLI)
 h, xmax = (1.,10.)
 x = np.arange(-xmax,xmax+h,h)#x-grid
 xx= np.arange(-xmax-h/20,xmax+h/20+h/10,h/10)#Grid for plotting
-v = np.where(abs(x) < 3,1,0) # The hat function
 fig = plt.figure(figsize =(24,8))
-ax =plt.axes()
-plt.plot(x,v)# plot the triangle function
-p =np.zeros(len(xx))#array of zeros to add terms of BLI
-for i in range(0,len(x)):
-    p =p+ v[i]*np.sin(np.pi*(xx-x[i])/h)/(np.pi*(xx-x[i])/h)# terms of BLI
-print(p)
-plt.plot(xx,p,'x',color='k')    
+for plot_range in range(3):
+    if plot_range == 0:
+        v= np.where(abs(x) < 1e-6,1,0) # "impulse at origin.
+    elif plot_range == 1:
+        v= np.where(abs(x) <=3, 1, 0) # Hat function
+    elif plot_range == 2:
+        v= np.where(np.maximum(0,1.-np.abs(x)/3),1,0)*(1-np.abs(x)/3) #triangle function
+    plt.subplot(4,1,plot_range+1)
+    plt.plot(x,v)# plot the triangle function
+    p =np.zeros(len(xx))#array of zeros to add terms of BLI
+    for i in range(0,len(x)):
+        p =p+ v[i]*np.sin(np.pi*(xx-x[i])/h)/(np.pi*(xx-x[i])/h)# terms of BLI
+    plt.plot(xx,p,'x',color='k')
+    plt.ylim([-0.3,1.2])
 plt.show()
